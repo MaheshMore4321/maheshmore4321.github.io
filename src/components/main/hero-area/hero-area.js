36dde278b0c8df52e9e0ee81dd54cfd843f59fc5
Mahesh Manohar More <MaheshMore4321@gmail.com>
2020-05-25 03:06:28 +0530

    hero-area making dynamic

--- src/components/main/hero-area/hero-area.js
+++ src/components/main/hero-area/hero-area.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import * as api from '../../constant/constant';
 import axios from 'axios'; 
-
+import parse from 'html-react-parser';
 import HeroAreaSlid from './hero-area_slide'
 
 class HeroArea extends Component {
@@ -11,13 +11,14 @@ class HeroArea extends Component {
 		this.state = {
       heroarea_: []
     }
+
+    //this.getData = this.getData.bind();
   }
 
   componentDidMount(){
 		axios.get(api.heroarea_api)
 		.then(response =>{
       this.setState({heroarea_: response.data});
-      this.setState({flag: true});
 		})
 		.catch(error =>{
 			console.log(error);
@@ -31,15 +32,7 @@ class HeroArea extends Component {
         <section id="sym-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <HeroAreaSlid
-                 data = {heroarea_[0]}
-              />
-              <HeroAreaSlid
-                data = {heroarea_[1]}
-              />
-              <HeroAreaSlid
-                data = {heroarea_[2]}
-              />
+              { getData(heroarea_) }
             </ul>
           </div>
         </section>
@@ -48,3 +41,63 @@ class HeroArea extends Component {
   }
 }
 export default HeroArea;
+
+function getData(dataS) {
+  console.log(dataS);
+  return dataS.map(function (data) {
+      return (
+        <li style={{backgroundImage:`url(${data.backgroundImg})`}}>
+        <div className="overlay"></div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+              <div className="slider-text-inner js-fullheight">
+                <div className="desc">
+                  <h1>
+                  {
+                    data && data.headerLine !== undefined &&
+                    parse(data.headerLine)
+                  }
+                  </h1>
+                  <p><a className="btn btn-primary btn-learn" href={data && data.link !== undefined && data.link} target="_blank" rel="noopener noreferrer">
+                  {
+                    data && data.text !== undefined &&
+                    parse(data.text)
+                  }
+                  <i className={data && data.icon !== undefined && data.icon}></i></a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li> 
+      )
+    }
+  )
+}
+function Components(data){
+  return(
+        <>
+        
+        </>
+  );
+  // '<li style=backgroundImage=url('+data.backgroundImg+')>'+
+  //         '<div className="overlay"></div>'+
+  //         '<div className="container-fluid">'+
+  //           '<div className="row">'+
+  //             '<div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">'+
+  //               '<div className="slider-text-inner js-fullheight">'+
+  //                 '<div className="desc">'+
+  //                   '<h1>'+
+  //                     data.headerLine
+  //                   + '</h1>'+
+  //                   '<p><a className="btn btn-primary btn-learn" href='+data.link+' target="_blank" rel="noopener noreferrer">'+
+  //                     data.text
+  //                   + '<i className='+data.text+'></i></a></p>'+
+  //                 '</div>'+
+  //               '</div>'+
+  //             '</div>'+
+  //           '</div>'+
+  //         '</div>'+
+  //       '</li>';
+}
