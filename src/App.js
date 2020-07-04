970ab3f29776471145f63d4e140978aca6d29d5f
Mahesh Manohar More <MaheshMore4321@gmail.com>
2020-07-05 00:01:19 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import { Router, Route, browserHistory } from 'react-router' 
 
 import Sidebar from './components/sidebar/sidebar'
-import Main from './components/main/main'
+import Main from './components/main/main' 
 
-class App extends Component { 
+class App extends Component {
   render() { 
     return (
       <div id="sym-page">
         <a href="# " className="js-sym-nav-toggle sym-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
         <Sidebar/>
         <Main/>
-        <Router history = {browserHistory}>
-          <Route path = "/undefined/" component = {Main}></Route>
-        </Router>    
       </div>
     );
   }
