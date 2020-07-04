a1a5065fe7ad11a34ca0e7d29e81d86aec0be2bc
Mahesh Manohar More <MaheshMore4321@gmail.com>
2020-07-05 00:09:18 +0530

    new v3 version added

---
+++ precache-manifest.fb2d1c56f248e395495295c55f43a686.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "49c07f005397244aebf2",
+    "url": "/static/js/main.49c07f00.chunk.js"
+  },
+  {
+    "revision": "68e1d395ce8741375ccd",
+    "url": "/static/js/2.68e1d395.chunk.js"
+  },
+  {
+    "revision": "4d5f538e8130195a9ff07c163f8e58f4",
+    "url": "/index.html"
+  }
+];
