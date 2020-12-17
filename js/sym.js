7e44e27d838cf1ad09a5f7e9d9d03c24f05da576
Mahesh More <MaheshMore4321@gmail.com>
2020-12-18 03:06:21 +0530

    UI Updated Version Added

--- js/sym.js
+++ js/sym.js
@@ -8,3 +8,31 @@ $('.js-sym-nav-toggle').on('click', function(event){
 		$('body').addClass('offcanvas');
 	}
 });
+
+$('#mc').resizable();
+$('#md').draggable();
+$('#myModal').on('show.bs.modal', function () {
+	$(this).find('.modal-body').css({
+		'max-height':'100%'
+	});
+});
+
+$('#clickNeedToCopy').on('click', function(event){
+	event.preventDefault();
+	CopyToClipboard('divNeedToCopy');
+});
+$('#clickNeedToCopyb').on('click', function(event){
+	event.preventDefault();
+	CopyToClipboard('divNeedToCopyb');
+});
+
+CopyToClipboard = (containerid) => {
+	CopyTextToClipboard(document.getElementById(containerid).innerText);
+}
+CopyTextToClipboard = (text) => {
+	navigator.clipboard.writeText(text).then(function() {
+		alert("Copying to clipboard was successful!");
+	}, function(err) {
+		console.error('Async: Could not copy text: ', err);
+	});
+}
